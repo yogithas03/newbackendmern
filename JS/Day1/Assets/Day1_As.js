4094d8437cb01c2f1a26e9a43c75d172ac7d37f9
yogitha03 <yogithasoundararajan03@gmail.com>
2026-06-03 10:48:05 +0530

    Updated code

--- JS/Day1/Assets/Day1_As.js
+++ JS/Day1/Assets/Day1_As.js
@@ -1,109 +1,110 @@
 
 // Task 1: Variable Declaration & Reassignment
 
-var personName="stark";
-console.log("var before:",personName)
-personName="pepper";
-console.log("var after:",personName)
+// var personName="stark";
+// console.log("var before:",personName)
+// personName="pepper";
+// console.log("var after:",personName)
 
-let girlAge=25;
-console.log("let before:",girlAge)
-girlAge=27;
-console.log("let after:",girlAge)
+// let girlAge=25;
+// console.log("let before:",girlAge)
+// girlAge=27;
+// console.log("let after:",girlAge)
 
-const companyName="Objectways";
-console.log("const before:",companyName)
-companyName="blueScope";
-console.log("const after:",companyName)
+// const companyName="Objectways";
+// console.log("const before:",companyName)
+// companyName="blueScope";
+// console.log("const after:",companyName)
 
-console.log('var and let can reassigned and redeclared but const cannot reassigned and redeclared after initialization.')
+// console.log('var and let can reassigned and redeclared but const cannot reassigned and redeclared after initialization.')
 
 // Task 2: Scope Challenge
 
-var globalVar = "I am Global";
+// var globalVar = "I am Global";
 
-function testScope() {
+// function testScope() {
  
-  var functionVar = "I am Function Scoped";
+//   var functionVar = "I am Function Scoped";
 
-  if (true) {
+//   if (true) {
    
-    let blockVar = "I am Block Scoped";
+//     let blockVar = "I am Block Scoped";
 
-    console.log(globalVar);   
-    console.log(functionVar); 
-    console.log(blockVar);   
-  }
+//     console.log(globalVar);   
+//     console.log(functionVar); 
+//     console.log(blockVar);   
+//   }
 
-  console.log(globalVar);  
-  console.log(functionVar); 
-  console.log(blockVar); 
-}
+  // console.log(globalVar);  
+  // console.log(functionVar); 
+  // console.log(blockVar); 
+// }
 
-testScope();
+// testScope();
 
-console.log(globalVar);   
-console.log(functionVar); 
-console.log(blockVar);   
+// console.log(globalVar);   
+// console.log(functionVar); 
+// console.log(blockVar);   
 
-// Task 3: Lexical Scope
+// // Task 3: Lexical Scope
 
-const fruit =()=>{
-      let fruitName = "Apple";
-const flesh =()=>{
-      let fleshName = "mesocarp";
-const seed=()=>{
-    let seedName = "pips";
+// const fruit =()=>{
+//       let fruitName = "Apple";
+// const flesh =()=>{
+//       let fleshName = "mesocarp";
+// const seed=()=>{
+//     let seedName = "pips";
 
-    console.log("friut:", fruitName);
-    console.log("flesh:",fleshName);
-    console.log("seed:", seedName);
-      };
-seed();
-    };
-flesh();
-  };
-fruit();
+//     console.log("friut:", fruitName);
+//     console.log("flesh:",fleshName);
+//     console.log("seed:", seedName);
+//       };
+// seed();
+//     };
+// flesh();
+//   };
+// fruit();
 
 // Task 4: Scope Chain Search
-const company = "SLA";
+// const company = "SLA";
 
-const level1 = () => {
-  const department = "Development";
+// const level1 = () => {
+//   const department = "Development";
 
-  const level2 = () => {
-    const team = "Frontend";
+//   const level2 = () => {
+//     const team = "Frontend";
 
-    const level3 = () => {
-      console.log("Company:", company);
-      console.log("Department:", department);
-      console.log("Team:", team);
-    };
+//     const level3 = () => {
+//       console.log("Company:", company);
+//       console.log("Department:", department);
+//       console.log("Team:", team);
+//     };
 
-    level3();
-  };
+//     level3();
+//   };
 
-  level2();
-};
+//   level2();
+// };
 
-level1();
+// level1();
 
-// Task 5: Hoisting Prediction
+// // Task 5: Hoisting Prediction
 console.log(a);
-var a = 100;
+var a = 100;  //undefined
+
 
 
 // Task 6: TDZ Investigation
 
-console.log(username);
-let username = "Sudhan";
+// console.log(username);
+// let username = "Sudhan";
 
 // Task 7: Execution Context
-const greet = () => {
-  console.log("Hello");
-};
+// const greet = () => {
+//   console.log("Hello");
+// };
 
-greet();
+// greet();
 
 // Task 8: Type Conversion Challenge
 
@@ -123,81 +124,81 @@ console.log(String(500)); //it covert the number into string 500
 
 //Primitive 
 //store single value,stored directly in the stack memory
-let name = "Samitha";          // String
-let age = 25;                 // Number
-let isStudent = true;         // Boolean
-let city;                     // Undefined
-let data = null;              // Null
-let bigNum = 1234567890n; // BigInt
+// let name = "Samitha";          // String
+// let age = 25;                 // Number
+// let isStudent = true;         // Boolean
+// let city;                     // Undefined
+// let data = null;              // Null
+// let bigNum = 1234567890n; // BigInt
 
 //Non-Primitive
 // can store multiple values,can be modify
-let person = {                // Object
-  name: "gomathi",
-  age: 25
-};
+// let person = {                // Object
+//   name: "gomathi",
+//   age: 25
+// };
 
-let colors = ["Red", "Blue"]; // Array
+// let colors = ["Red", "Blue"]; // Array
 
-let greet = () => {           // Function
-  console.log("Hello");
-};
+// let greet = () => {           // Function
+//   console.log("Hello");
+// };
 
 // Task 10: Complete Interview Scenario
 
 
-console.log(company); // undefined //hoisting
-var company = "TechWorld";
+// console.log(company); // undefined //hoisting
+// var company = "TechWorld";
 
 //functionScope
-const outer = () => {  
-  let department = "Frontend";
+// const outer = () => {  
+//   let department = "Frontend";
   
-  //blockScope
-  if (true) {
-    const team = "React Team";
+//   //blockScope
+//   if (true) {
+//     const team = "React Team";
 
-    const inner = () => {
-      console.log("Company:", company);
-      console.log("Department:", department);
-      console.log("Team:", team);
+//     const inner = () => {
+//       // console.log("Company:", company);
+//       console.log("Department:", department);
+//       console.log("Team:", team);
 
-      // Type Conversion
-      console.log("100" + 20); //string concatenation "100"+20=10020
-      console.log(Number("50") + 10); //it convert the string to number 50+10=60
-    };
-    inner(); //functioncall
-  }
+//       // Type Conversion
+//       console.log("100" + 20); //string concatenation "100"+20=10020
+//       console.log(Number("50") + 10); //it convert the string to number 50+10=60
+//     };
+//     inner(); //functioncall
+//   }
    
-  // TDZ Example
- console.log(employee); // ReferenceError let and const cannot accessed before initialzation
-  let employee = "Manisha";
-  console.log("Employee:", employee);
-};
-outer();
+//   // TDZ Example
+//  console.log(employee); // ReferenceError let and const cannot accessed before initialzation
+//   let employee = "Manisha";
+//   console.log("Employee:", employee);
+// };
+// outer();
 
 // Bonus Interview Task 
 
-var a = 10;
+// var a = 10;
 
-const outer = () => {
+// const outer = () => {
 
-  let b = 20;
+//   let b = 20;
 
-  const inner = () => {
+//   const inner = () => {
 
-    console.log(a); //10
+//     console.log(a); //10
 
-    console.log(b);//20
+//     console.log(b);//20
 
-    console.log("5" + 2);//string concatenation "5"+2=52
+//     console.log("5" + 2);//string concatenation "5"+2=52
 
-  };
+//   };
 
-  inner();
+//   inner();
 
-};
+// };
 
-outer();
+// outer();
 
 
