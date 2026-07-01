091df0f2f6f3fdc689ded38465949f924c981afe
yogitha03 <yogithasoundararajan03@gmail.com>
2026-07-01 12:37:03 +0530

    updated

---
+++ React/Day13/searchprocess/src/App.jsx
@@ -0,0 +1,13 @@
+import Student from "./components/Student"
+
+
+const App = () => {
+  return (
+    <>
+    <Student/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ React/Day13/searchprocess/src/components/Student.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+
+const Student = () => {
+ 
+
+    const studentDatas = [ { id: 1, name: "Sudhan" },
+    { id: 2, name: "Sudhakar" },
+    { id: 3, name: "Rahul" },
+    { id: 4, name: "Priya" },
+    { id: 5, name: "Karthik" },
+    { id: 6, name: "Suresh" }]
+
+    const [search,setSearch]=useState("")
+
+    const handleChange = (e)=>{
+        setSearch(e.target.value)
+    }
+    
+    const filteredStudents = studentDatas.filter((student) =>
+    student.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+
+
+
+
+  return (
+    <>
+    <h1>Student Search</h1>
+
+    <input type="text" placeholder="search student" value={search} onChange={handleChange}/>
+
+      {filteredStudents.length > 0 ? (
+        <ul>
+          {filteredStudents.map((student) => (
+            <li key={student.id}>{student.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No Student Found</p>
+      )}
+
+      
+    </>
+  )
+}
+
+export default Student
